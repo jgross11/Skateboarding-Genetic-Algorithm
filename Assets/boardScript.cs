deb47b9161bdeb84b7a30aa00caec436a6265763
jgross11 <jgross11@ycp.edu>
2020-12-05 18:03:36 -0500

    Initial Commit

---
+++ Assets/boardScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardScript : MonoBehaviour
+{
+    // fields
+    private Vector3 initialPos;
+    private Quaternion initialRotation;
+    private Rigidbody rgbd;
+    private float runCounter;
+    public float runTime;
+    private int wheelsOnGround;
+    private const int NUM_WHEELS = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+        initialRotation = transform.rotation;
+        runCounter = 0;
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(runCounter < runTime){
+            runCounter += Time.deltaTime;
+        }
+        else{
+            reset();
+            runCounter = 0;
+        }
+
+    }
+
+    public void UpdateWheelCount(int value){
+        wheelsOnGround += value;
+        Debug.Log("board " + GetInstanceID() + " has " + wheelsOnGround + " wheel(s) on ground");
+    }
+
+    void reset()
+    {
+        // disable physics to wipe velocity, torque, etc.
+        rgbd.isKinematic = true;
+
+        // reset position to start
+        transform.position = initialPos;
+
+        // reset rotation to start
+        transform.rotation = initialRotation;
+
+        // enable physics for next run
+        rgbd.isKinematic = false;
+    }
+}
---
+++ Assets/groundScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundScript : MonoBehaviour
+{
+
+    // map storing all created boards' ID as key and script as value
+    private Dictionary<int, boardScript> boardMap;
+
+    // board GameObject to populate field
+    public GameObject board;
+
+    // number of generations to run experiment
+    public int numberOfGenerations;
+
+    // number of specimen per generation
+    public int numberOfSpecimens;
+
+    // mutation percentage
+    public float mutationPercentage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardMap = new Dictionary<int, boardScript>();
+        RunExperiment();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Collider col = collision.collider;
+        if(col.tag == "wheel"){
+
+            // get board instance ID
+            int boardID = col.transform.parent.GetInstanceID();
+
+            // if no reference to this board exists in map, create it 
+            if(!boardMap.ContainsKey(boardID)){
+                boardScript bs = col.gameObject.GetComponentInParent(typeof(boardScript)) as boardScript;
+                boardMap.Add(boardID, bs);
+                Debug.Log("new board in map");
+            }
+
+            // increment number of wheels on ground for this board
+            boardMap[boardID].UpdateWheelCount(1);
+        }
+        
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Collider col = collision.collider;
+        if(col.tag == "wheel"){
+
+            // get board instance ID
+            int boardID = col.transform.parent.GetInstanceID();
+
+            // if no reference to this board exists in map, create it 
+            if(!boardMap.ContainsKey(boardID)){
+                boardScript bs = col.gameObject.GetComponentInParent(typeof(boardScript)) as boardScript;
+                boardMap.Add(boardID, bs);
+                Debug.Log("new board in map");
+            }
+
+            // decrement number of wheels on ground for this board
+            boardMap[boardID].UpdateWheelCount(-1);
+        }
+    }
+
+
+    // initializes the experiment
+    void RunExperiment(){
+        
+    }
+}
