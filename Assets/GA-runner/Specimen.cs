1947b4d4d842ae61a4df37070705cfd7758da1ae
jgross11 <jgross11@ycp.edu>
2020-12-07 14:35:48 -0500

    Created Specimen
    Created abstract class representing a specimen

---
+++ Assets/GA-runner/Specimen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Specimen : MonoBehaviour
+{
+    // the fitness function to use when evaluating the specimen's performance
+    private FitnessFunction fitnessFunction;
+    
+    // the board this specimen acts through (setting up foot positioning, pressures, etc.)
+    private GameObject board;
+
+    // the fitness value of this board
+    private double fitnessValue;
+
+    // the identifier for this specimen in its generation
+    private int specimenID;
+
+    // initializes the appropriate parameters of this specimen's board
+    public abstract void initModel();
+
+    // calculates this specimen's fitness
+    public double calculateFitness(){
+        fitnessValue = fitnessFunction.getFitness();
+        return fitnessValue;
+    }
+
+    // returns the fitness of this specimen
+    public double getFitness(){
+        return fitnessValue;
+    }
+}
