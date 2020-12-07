cbe7718aeb000e2f1ac4cf8fceea4fd7ae9c25fa
jgross11 <jgross11@ycp.edu>
2020-12-07 14:35:16 -0500

    Created FitnessFunction
    Created abstract class to represent a fitness function

---
+++ Assets/GA-runner/FitnessFunction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FitnessFunction
+{
+
+    // indicates how to calculate a fitness function's value
+    public abstract double getFitness();
+}
