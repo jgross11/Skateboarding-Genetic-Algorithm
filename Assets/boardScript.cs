5a625f9737516c680f5c49392c31cc3857e69090
jgross11 <jgross11@ycp.edu>
2020-12-11 23:30:05 -0500

    Update boardScript.cs
    Added logic to determine when board is in air / on ground, added BoardData generation logic

--- Assets/boardScript.cs
+++ Assets/boardScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class boardScript : MonoBehaviour
 {
@@ -12,6 +13,18 @@ public class boardScript : MonoBehaviour
     public float runTime;
     private int wheelsOnGround;
     private const int NUM_WHEELS = 4;
+    private bool boardOnGround = true;
+    private float airtime = 0.0f;
+    private int boardID = -1;
+    private float fitness = 0.0f;
+    private static float bestAirtime = 0.0f;
+    private List<Vector3> rotationsInAir;
+    private List<Vector3> positionsInAir;
+    private BoardData boardData;
+    private float firstLastWheelDelta = 0.0f;
+    private bool isAWheelOnGround = false;
+    public Specimen specimen;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +33,8 @@ public class boardScript : MonoBehaviour
         initialRotation = transform.rotation;
         runCounter = 0;
         rgbd = GetComponent<Rigidbody>();
+        rotationsInAir = new List<Vector3>();
+        positionsInAir = new List<Vector3>();
     }
 
     // Update is called once per frame
@@ -33,6 +48,46 @@ public class boardScript : MonoBehaviour
             runCounter = 0;
         }
 
+        // all wheels just hit ground
+        if(!boardOnGround && wheelsOnGround == NUM_WHEELS){
+            boardOnGround = true; 
+            Debug.Log("board " + GetInstanceID() + " in air for " + airtime + " seconds");
+            Debug.Log("time between first and last wheel hitting ground was " + firstLastWheelDelta + " seconds");
+            
+            /*
+            foreach(Vector3 info in rotationsInAir){
+                Debug.Log(info);
+            }
+            foreach(Vector3 info in positionsInAir)
+            {
+                Debug.Log(info);
+            }
+            */
+            boardData = GenerateBoardData();
+            Debug.Log(boardData.ToString());
+            Debug.Log("fitness: " + specimen.CalculateFitness(boardData));
+            Debug.Log(boardData.minXAngle);
+            Debug.Log(boardData.minYAngle);
+            Debug.Log(boardData.minZAngle);
+            airtime = 0;
+            rotationsInAir = new List<Vector3>();
+            positionsInAir = new List<Vector3>();
+
+        }
+        else if(isAWheelOnGround){
+            firstLastWheelDelta += Time.deltaTime;
+        }
+        // all wheels just left ground
+        else if(boardOnGround && wheelsOnGround == 0){
+            Debug.Log("board " + GetInstanceID() + " in air");
+            boardOnGround = false;
+            firstLastWheelDelta = 0;
+        // all wheels have been off ground for some time
+        } else if(wheelsOnGround == 0){
+            airtime += Time.deltaTime;
+            rotationsInAir.Add(transform.rotation.eulerAngles);
+            positionsInAir.Add(transform.position);
+        }
     }
 
     /*
@@ -43,7 +98,7 @@ public class boardScript : MonoBehaviour
     */
     public void UpdateWheelCount(int value){
         wheelsOnGround += value;
-        Debug.Log("board " + GetInstanceID() + " has " + wheelsOnGround + " wheel(s) on ground");
+        isAWheelOnGround = wheelsOnGround > 0;
     }
 
     void reset()
@@ -59,5 +114,53 @@ public class boardScript : MonoBehaviour
 
         // enable physics for next run
         rgbd.isKinematic = false;
+    }
+
+    private float calculateFitness(){
+        return 0.0f;
+    }
+
+    private BoardData GenerateBoardData(){
+        BoardData data = new BoardData();
+
+        // calculate maximum rotation variance 
+        // TODO this probably isn't correct.
+        float currentMaxX = 0;
+        float currentMaxY = 0;
+        float currentMaxZ = 0;
+        foreach(Vector3 rotData in rotationsInAir)
+        {
+            float currentX = (float)Math.Sqrt(Math.Abs(rotData.x));
+            float currentY = (float)Math.Sqrt(Math.Abs(rotData.y));
+            float currentZ = (float)Math.Sqrt(Math.Abs(rotData.z));
+
+            if(currentX - initialRotation.eulerAngles.x > currentMaxX){
+                currentMaxX = (float) Math.Abs(currentX - initialRotation.eulerAngles.x);
+            }
+            if(currentY - initialRotation.eulerAngles.y > currentMaxY){
+                currentMaxY = (float) Math.Abs(currentY - initialRotation.eulerAngles.y);
+            }
+            if(currentZ - initialRotation.eulerAngles.z > currentMaxZ){
+                currentMaxZ = (float) Math.Abs(currentZ - initialRotation.eulerAngles.z);
+            }
+        }
+        data.minXAngle = currentMaxX;
+        data.minYAngle = currentMaxY;
+        data.minZAngle = currentMaxZ;
+
+        // calculate highest point during airtime
+        float highestHeight = 0;
+        foreach(Vector3 pos in positionsInAir)
+        {
+            float currentHeight = (float)Math.Abs(pos.y - initialPos.y);
+            if(currentHeight > highestHeight){
+                highestHeight = currentHeight;
+            }
+        }
+        data.height = highestHeight;
+
+        // assign first/last wheel time
+        data.firstLastWheelDelta = firstLastWheelDelta;
+        return data;
     }
 }
