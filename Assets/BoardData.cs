d6c86fbe83866b0df3cad67f93a4ddcb0d0d07a5
jgross11 <jgross11@ycp.edu>
2020-12-11 23:29:30 -0500

    Created BoardData.cs
    Created class to contain a summary of all board data used in calculating fitness of trick attempt

---
+++ Assets/BoardData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardData
+{
+    // highest point center of board got in run
+    public float height;
+
+    // TODO end and start position of both feet
+
+    // time between first and last wheel hitting ground (not / sketchy)
+    public float firstLastWheelDelta;
+
+    // minimum angle of rotation experienced on X axis
+    public float minXAngle;
+
+    // minimum angle of rotation experienced on Y axis
+    public float minYAngle;
+
+    // minimum angle of rotation experienced on Z axis
+    public float minZAngle;
+
+    // TODO max distance between two feet and board
+
+    public override string ToString(){
+        string result = "";
+        result += "max height: " + height;
+        result += "\nfirst / last wheel time difference: " + firstLastWheelDelta;
+        result += "\nmax X angle: " + minXAngle;
+        result += "\nmax Y angle: " + minYAngle;
+        result += "\nmax Z angle: " + minZAngle;
+        return result;
+    }
+}
