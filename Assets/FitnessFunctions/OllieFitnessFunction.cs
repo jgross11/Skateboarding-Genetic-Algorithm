dd994cb252eebd2323287118b66cc72d5d2e831d
jgross11 <jgross11@ycp.edu>
2020-12-12 15:32:39 -0500

    Update OllieFitnessFunction.cs
    Added logic to handle case where both feet don't land on board

--- Assets/FitnessFunctions/OllieFitnessFunction.cs
+++ Assets/FitnessFunctions/OllieFitnessFunction.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class OllieFitnessFunction : FitnessFunction
 {
+    public float highOllieHeight = 1.0f;
     // ollie fitness function, what a doozy
     public override float GetFitness(BoardData data){
-        // height = 0.25 weight
-        // firstLastWheelDelta = 0.25 weight
-        // minY = 0.25 weight
-        // minZ = 0.25 weight
+        // height = 0.25 weight - maximize
+        // firstLastWheelDelta = 0.25 weight - minimize
+        // minY = 0.25 weight - minimize
+        // minZ = 0.25 weight - minimize
+        // must have both feet on board to consider the trick completed
+        // so return 0 if both feet aren't on board after trick is executed
         // TODO determine max height
-        return (float)(data.height / 1.0 * 0.25 + 
-        (1.0 / (data.firstLastWheelDelta + 1)) * 0.25 + 
-        (1.0 / (data.minYAngle + 1)) * 0.25 +
-        (1.0 / (data.minZAngle + 1)) * 0.25);
+        return data.bothFeetOnBoard ? (float)((data.height / highOllieHeight * 0.25) + 
+        ((1.0 / (data.firstLastWheelDelta + 1)) * 0.25) + 
+        ((1.0 / (data.minYAngle + 1)) * 0.25) +
+        ((1.0 / (data.minZAngle + 1)) * 0.25))
+        : 0;
     }
 }
