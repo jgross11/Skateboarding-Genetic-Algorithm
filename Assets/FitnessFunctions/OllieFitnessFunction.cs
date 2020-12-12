b1068c4d15484a297093dbc79dac5a2828a1c6a8
jgross11 <jgross11@ycp.edu>
2020-12-11 23:31:50 -0500

    Created OllieFitnessFunction
    Started work on basic ollie fitness function with idea that a good ollie
    - minimizes 2/3 axis rotation numbers
    - maximizes height
    - minimizes time between first and last wheel hitting ground

---
+++ Assets/FitnessFunctions/OllieFitnessFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OllieFitnessFunction : FitnessFunction
+{
+    // ollie fitness function, what a doozy
+    public override float GetFitness(BoardData data){
+        // height = 0.25 weight
+        // firstLastWheelDelta = 0.25 weight
+        // minY = 0.25 weight
+        // minZ = 0.25 weight
+        // TODO determine max height
+        return (float)(data.height / 1.0 * 0.25 + 
+        (1.0 / (data.firstLastWheelDelta + 1)) * 0.25 + 
+        (1.0 / (data.minYAngle + 1)) * 0.25 +
+        (1.0 / (data.minZAngle + 1)) * 0.25);
+    }
+}
