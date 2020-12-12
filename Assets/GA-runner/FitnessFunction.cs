9a2d5f37437aacc34c3f3295568c74527babce12
jgross11 <jgross11@ycp.edu>
2020-12-11 23:30:44 -0500

    Update FitnessFunction
    Added BoardData parameter to abstract function

--- Assets/GA-runner/FitnessFunction.cs
+++ Assets/GA-runner/FitnessFunction.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class FitnessFunction
+public abstract class FitnessFunction : MonoBehaviour
 {
 
     // indicates how to calculate a fitness function's value
-    public abstract double getFitness();
+    public abstract float GetFitness(BoardData data);
 }
