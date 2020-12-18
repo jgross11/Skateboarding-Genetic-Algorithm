18f5b61258e9a70b5fb5927f025380895796e3f5
jgross11 <jgross11@ycp.edu>
2020-12-18 16:08:00 -0500

    Create Action.cs
    Created class to hold action information, rather than a 3-tuple

---
+++ Assets/Action.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action
+{
+    public Vector3 direction;
+    public bool isLeftFoot;
+    public float executionTime;
+
+    public Action(float time, Vector3 dir, bool foot){
+        direction = dir;
+        isLeftFoot = foot;
+        executionTime = time;
+    }
+
+    public override string ToString()
+    {
+        return "dir="+direction.ToString()+"|isLeftFoot="+isLeftFoot+"|time="+executionTime+"\n";
+    }
+}
