e4238c67877fcdc17dc404b7ee4c890b5c807508
jgross11 <jgross11@ycp.edu>
2020-12-19 20:45:21 -0500

    Update OllieFitnessFunction.cs
    Continued to mess with weights, had idea to not just minimize effectiveness of bad results, but to actively discourage them by having them negatively impact score

--- Assets/FitnessFunctions/OllieFitnessFunction.cs
+++ Assets/FitnessFunctions/OllieFitnessFunction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OllieFitnessFunction : FitnessFunction
 {
-    public float highOllieHeight = 1.0f;
+    public float highOllieHeight = 5.0f;
     // ollie fitness function, what a doozy
     public override float GetFitness(BoardData data){
         // height = 0.25 weight - maximize
@@ -14,9 +14,23 @@ public class OllieFitnessFunction : FitnessFunction
         // must have both feet on board to consider the trick completed
         // so return 0 if both feet aren't on board after trick is executed
         // TODO determine max height
+        /*
         return (float)((data.height / highOllieHeight * 0.03125) + 
         ((1.0 / (data.firstLastWheelDelta + 1)) * 0.445) + 
         ((1.0 / (data.minYAngle + 1)) * 0.245) +
-        ((1.0 / (data.minZAngle + 1)) * 0.245)) + (data.feetOnBoard * 0.45f);
+        ((1.0 / (data.minZAngle + 1)) * 0.245)) + 
+        (data.feetOnBoard * 1000.45f0) +
+        ((1.0f / (data.leftFootVariance+1))) +
+        ((1.0f / (data.rightFootVariance+1)));
+        */
+        return (
+            
+            data.height * 0.3f - 3.0f + // maximize height of ollie
+            (-data.boardPositionVariance*2 + 1) * 0.15f + // minimize non-vertical distance of board and punish when excessively far away
+            (-data.firstLastWheelDelta + 1) * 0.05f + // minimize time between first/last wheel on ground and punish when excessively large period of time
+            (-data.leftFootVariance + 1) * 0.15f + // minimize distance left foot travels and punish when excessively far away
+            (-data.rightFootVariance + 1) * 0.15f + // minimize distance right foot travels and punish when excessively far away
+            (data.feetOnBoard == 2 ? 5.0f : data.feetOnBoard == 1 ? 0.0f : -2.0f) // give appropriate weight depending on number of feet on board 
+        );
     }
 }
