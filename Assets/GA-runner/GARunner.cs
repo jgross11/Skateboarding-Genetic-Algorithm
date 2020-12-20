b5efb67c4fe49990be712e7db10a53c20a4a910e
jgross11 <jgross11@ycp.edu>
2020-12-19 20:46:55 -0500

    Update GARunner.cs
    Separated Specimen from their GameObject to display most fit Specimen's trick attempt next to camera, implemented new Action class over Tuple

--- Assets/GA-runner/GARunner.cs
+++ Assets/GA-runner/GARunner.cs
@@ -48,22 +48,27 @@ public class GARunner : MonoBehaviour
     public float maxMovementComponentValue;
 
     // population array
-    public GameObject[] population;
+    public GameObject[] populationGOArr;
+
+    // population specimen information
+    public Specimen[] population;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.transform.localScale = new Vector3(200 * numberOfSpecimens, 1, 200);
-        Time.timeScale *= timeMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Time.timeScale = timeMultiplier;
         durationOfCurrentAttempt += Time.deltaTime;
         // if it is time to end current attempt
         if(durationOfCurrentAttempt > durationPerAttempt){
+            ResetPopulation();
             SortPopulationByFitness();
             GenerateNextGeneration();
             durationOfCurrentAttempt = 0;
@@ -71,36 +76,46 @@ public class GARunner : MonoBehaviour
 
     }
 
+    public void ResetPopulation(){
+        foreach(Specimen spec in population)
+        {
+            spec.Reset();
+        }
+    }
+
     // initializes the experiment
     public void RunExperiment(){
         
         // create initial population
-        population = new GameObject[numberOfSpecimens];
+        populationGOArr = new GameObject[numberOfSpecimens];
+        population = new Specimen[numberOfSpecimens];
         int xOffset = 0;
         for(int popIndex = 0; popIndex < numberOfSpecimens; popIndex++){
             // create gameobjects that will contain and perform movements
             GameObject go = Instantiate(specimenType, new Vector3(xOffset++ * 100, -3, 0), new Quaternion(0, 0, 0, 1));
             
-            go.name = "Generation 1 Board " + (popIndex + 1);
+            go.name = "Board " + (popIndex + 1);
 
             // generate random movement tuple and assign to game object's specimen
-            Tuple<float, Vector3, bool>[] actions = new Tuple<float, Vector3, bool>[numberOfActions];
+            Action[] actions = new Action[numberOfActions];
 
             float currentTimeMin = 0;
             for(int i = 0; i < numberOfActions; i++)
             {
-                float newTimeMin = r.Range(currentTimeMin, maxTime);
-                actions[i] = CreateNewTrait(newTimeMin);
-                currentTimeMin = newTimeMin;
+                actions[i] = CreateNewTrait(currentTimeMin);
+                currentTimeMin = actions[i].executionTime;
             }
-            go.GetComponent<Specimen>().setActionTuple(actions);
-            population[popIndex] = go;
+
+            population[popIndex] = go.GetComponent<Specimen>();
+            population[popIndex].SetActions(actions);
+            population[popIndex].specimenID = popIndex+1;
+            populationGOArr[popIndex] = go;
         }
         
     }
 
-    private Tuple<float, Vector3, bool> CreateNewTrait(float minTime){
-        return Tuple.Create(r.Range(minTime, maxTime),                             // time to execute action at
+    private Action CreateNewTrait(float minTime){
+        return new Action(r.Range(minTime, maxTime),                             // time to execute action at
         new Vector3(r.Range(minMovementComponentValue, maxMovementComponentValue),  // x component direction
                     r.Range(minMovementComponentValue, maxMovementComponentValue),  // y component direction
                     r.Range(minMovementComponentValue, maxMovementComponentValue)), // z component direction
@@ -109,20 +124,37 @@ public class GARunner : MonoBehaviour
 
     public void SortPopulationByFitness(){
         System.Array.Sort(population, compareByFitness);
-        Debug.Log("current fittest after sorting: " + population[0].GetComponent<Specimen>().GetFitness());
-        Debug.Log("fittest name: " + population[0].name);
-    }
-
-    public int compareByFitness(GameObject a, GameObject b){
-        return b.GetComponent<Specimen>().GetFitness().CompareTo(a.GetComponent<Specimen>().GetFitness());
-    }
-
-    // calculate the fitness of each specimen
-    void calculateFitnesses(){
-        foreach (GameObject spec in population){
-            // TODO fix this
-            // spec.CalculateFitness();
+        /*
+            Debug.Log("current fittest after sorting: " + population[0].ToString());
+            Debug.Log("least fittest after sorting: " + population[population.Length-1].ToString());
+            Debug.Log("most-least fit diff: " + (population[0].GetFitness() - population[population.Length-1].GetFitness()));
+        */
+        float averageFitness = 0.0f;
+        foreach(Specimen spec in population)
+        {
+            averageFitness += (float)spec.GetFitness();
         }
+        averageFitness /= (float)population.Length;
+        Debug.Log("Average fitness: " + averageFitness);
+        for(int i = 0; i < numberOfSpecimens; i++)
+        {
+            populationGOArr[i].GetComponent<Specimen>().SetActions(population[i].GetActions());
+            populationGOArr[i].GetComponent<Specimen>().specimenID = population[i].specimenID;
+        }
+        
+        // debug sorting
+        
+        /*
+        foreach(Specimen spec in population)
+        {
+            Debug.Log("Specimen name: " + spec.name + " | fitness: " + spec.GetFitness());
+        }
+        */
+
+    }
+
+    public int compareByFitness(Specimen a, Specimen b){
+        return b.GetFitness().CompareTo(a.GetFitness());
     }
 
     // determine the fittest specimen in the current population
@@ -145,14 +177,9 @@ public class GARunner : MonoBehaviour
 
     // TODO ensure next generation's specimen aren't overwriting current generation while breeding
     void GenerateNextGeneration(){
-        for(int i = 0; i < (int)(truncationPercentage * numberOfSpecimens); i++){
-            population[i].GetComponent<Specimen>().Reset();
-        }
         for(int i = (int)(truncationPercentage * numberOfSpecimens); i < numberOfSpecimens; i++){
             // Debug.Log(i + "th specimen before: " + population[i].GetComponent<Specimen>().GetActionTuple()[0].Item1);
-            Specimen newPop = population[i].GetComponent<Specimen>();
-            newPop.setActionTuple(breed(population[(int) r.Range(0, (truncationPercentage * numberOfSpecimens))].GetComponent<Specimen>(), population[(int) r.Range(0, (truncationPercentage * numberOfSpecimens))].GetComponent<Specimen>()));
-            newPop.Reset();
+            population[i].SetActions(breed(population[(int) r.Range(0, (truncationPercentage * numberOfSpecimens))], population[(int) r.Range(0, (truncationPercentage * numberOfSpecimens))]));
             // Debug.Log(i + "th specimen after: " + population[i].GetComponent<Specimen>().GetActionTuple()[0].Item1);
             // Debug.Log("Current fitnes: " + next[i].GetFitness());
         }
@@ -166,14 +193,14 @@ public class GARunner : MonoBehaviour
     }
 
     // TODO make this truly breed
-    private Tuple<float, Vector3, bool>[] breed(Specimen a, Specimen b){
-        Tuple<float, Vector3, bool>[] aActions = a.GetActionTuple();
-        Tuple<float, Vector3, bool>[] bActions = b.GetActionTuple();
-        Tuple<float, Vector3, bool>[] result = new Tuple<float, Vector3, bool>[numberOfActions];
+    private Action[] breed(Specimen a, Specimen b){
+        Action[] aActions = a.GetActions();
+        Action[] bActions = b.GetActions();
+        Action[] result = new Action[numberOfActions];
         for(int i = 0; i < numberOfActions; i++)
         {
             float mutationValue = r.value;
-            result[i] = (mutationValue < mutationPercentage) ? CreateNewTrait(i == 0 ? 0 : result[i-1].Item1) : ((int) Math.Round(r.value) == 0) ? aActions[i] : bActions[i];
+            result[i] = (mutationValue < mutationPercentage) ? CreateNewTrait(i == 0 ? 0 : result[i-1].executionTime) : ((int) Math.Round(r.value) == 0) ? aActions[i] : bActions[i];
             // Debug.Log("a"+i+": " + aActions[i].Item1 + ", " + aActions[i].Item2 + ", " + aActions[i].Item3);
             // Debug.Log("b"+i+": " + bActions[i].Item1 + ", " + bActions[i].Item2 + ", " + bActions[i].Item3);
             // Debug.Log("result "+i+": " + result[i].Item1 + ", " + result[i].Item2 + ", " + result[i].Item3);
@@ -183,11 +210,11 @@ public class GARunner : MonoBehaviour
         SortActionsByExecutionTime(result);
         return result;
     }
-    public void SortActionsByExecutionTime(Tuple<float, Vector3, bool>[] spec){
+    public void SortActionsByExecutionTime(Action[] spec){
         System.Array.Sort(spec, compareByExecutionTime);
     }
 
-    public int compareByExecutionTime(Tuple<float, Vector3, bool> a, Tuple<float, Vector3, bool> b){
-        return a.Item1.CompareTo(b.Item1);
+    public int compareByExecutionTime(Action a, Action b){
+        return a.executionTime.CompareTo(b.executionTime);
     }
 }
